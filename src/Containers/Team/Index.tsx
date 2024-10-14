8972abc575a723d252f48f7d3b85a15213f848da
Joao Lara <joaovdelara@gmail.com>
2024-10-14 09:33:37 -0300

    create team container and components

--- src/Containers/Team/Index.tsx
+++ src/Containers/Team/Index.tsx
@@ -3,37 +3,65 @@ import { Container, Row, Col } from 'react-bootstrap';
 import TeamCard from '../../components/teamCard';
 
 const Team: React.FC = () => {
+  const teamMembers = [
+    {
+      name: "Full Name 1",
+      designation: "Designation 1",
+      image: "src/assets/images/person_1.jpg",
+      facebookUrl: "#",
+      twitterUrl: "#",
+      instagramUrl: "#",
+    },
+    {
+      name: "Full Name 2",
+      designation: "Designation 2",
+      image: "src/assets/images/person_2.jpg",
+      facebookUrl: "#",
+      twitterUrl: "#",
+      instagramUrl: "#",
+    },
+    {
+      name: "Full Name 3",
+      designation: "Designation 3",
+      image: "src/assets/images/person_3.jpg",
+      facebookUrl: "#",
+      twitterUrl: "#",
+      instagramUrl: "#",
+    },
+    {
+      name: "Full Name 4",
+      designation: "Designation 4",
+      image: "src/assets/images/person_4.jpg",
+      facebookUrl: "#",
+      twitterUrl: "#",
+      instagramUrl: "#",
+    },
+    {
+      name: "Full Name 5",
+      designation: "Designation 5",
+      image: "src/assets/images/person_1.jpg",
+      facebookUrl: "#",
+      twitterUrl: "#",
+      instagramUrl: "#",
+    },
+  ];
+
   return (
     <Container>
       <div className="d-flex overflow-auto">
-        <Row className="flex-nowrap">
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
-          <Col xs={6} sm={6} md={3} lg={3}>
-            <TeamCard />
-          </Col>
+        <Row className="flex-nowrap my-5">
+          {teamMembers.map((member, index) => (
+            <Col key={index} xs={12} sm={6} md={3} lg={3}>
+              <TeamCard
+                name={member.name}
+                designation={member.designation}
+                image={member.image}
+                facebookUrl={member.facebookUrl}
+                twitterUrl={member.twitterUrl}
+                instagramUrl={member.instagramUrl}
+              />
+            </Col>
+          ))}
         </Row>
       </div>
     </Container>
--- src/components/teamCard/index.tsx
+++ src/components/teamCard/index.tsx
@@ -1,35 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
+import styles from './card.module.scss';
+
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
-import './card.module.scss'
+interface TeamCardProps {
+    name: string;
+    designation: string;
+    image: string;
+    facebookUrl: string;
+    twitterUrl: string;
+    instagramUrl: string;
+}
 
-const TeamCard: React.FC = () => {
-    const [hovered, setHovered] = useState(false); 
-
+const TeamCard: React.FC<TeamCardProps> = ({ name, designation, image, facebookUrl, twitterUrl, instagramUrl }) => {
     return (
-        <Card 
-            onMouseEnter={() => setHovered(true)} 
-            onMouseLeave={() => setHovered(false)} 
-            className="position-relative"
-        >
-            <Card.Img variant="top" src="src/assets/images/person_1.jpg" />
-            <Card.Body className='text-center'>
-                <Card.Title>Nome do Artista</Card.Title>
-            </Card.Body>
-            <Card.Footer className='text-center'>
-                {hovered ? (
-                    <div className="social-icons">
-                        <a href="#" className="icon"><FaFacebook /></a>
-                        <a href="#" className="icon"><FaTwitter /></a>
-                        <a href="#" className="icon"><FaInstagram /></a>
+        <Card className={`${styles.card} text-center border rounded wow fadeInUp`} data-wow-delay="0.1s">
+            <div className={styles['team-item']}>
+                <Card.Img variant="top" src={image} alt={`${name}`} className="img-fluid" />
+
+                <div className={styles['team-text']}>
+                    <div className={styles['team-title']}>
+                        <h5>{name}</h5>
+                        <span>{designation}</span>
                     </div>
-                ) : (
-                    <small className="text-muted">Profissão</small>
-                )}
-            </Card.Footer>
+                    <div className={styles['team-social']}>
+                        <a className={`${styles.btn} btn btn-square btn-light rounded-circle`} 
+                        href={facebookUrl}>
+                            <FaFacebook />
+                        </a>
+                        <a className={`${styles.btn} btn btn-square btn-light rounded-circle`} 
+                        href={twitterUrl}>
+                            <FaTwitter />
+                        </a>
+                        <a className={`${styles.btn} btn btn-square btn-light rounded-circle`} 
+                        href={instagramUrl}>
+                            <FaInstagram />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </Card>
-    )
+    );
 }
 
 export default TeamCard;
